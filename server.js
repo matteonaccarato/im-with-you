63db3e775ccb5ae0b1ee2ce770ed6d8259386118
Matteo Naccarato <matteo.naccarato@itiscuneo.eu>
2021-05-24 22:10:11 +0200

    service commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
 const app = require('./app');
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`I'm with you ♥ | Server started on port ${PORT}`);
 })
